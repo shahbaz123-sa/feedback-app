1f2f7db388e45f2e031085a873c4d6deff11fd76
shahbaz123-sa <ahmadshahbazkz@gmail.com>
2024-03-25 16:49:46 +0500

    merging code

---
+++ storage/framework/views/538c007b12af38706ea916e3d86facc5.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Feedback Application</title>
+    <!-- Include your CSS styles here -->
+
+    <link rel="stylesheet" type="text/css" href="<?php echo asset(''); ?>/public/app-assets/vendors/css/vendors.min.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo asset(''); ?>/public/app-assets/vendors/css/forms/wizard/bs-stepper.min.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo asset(''); ?>/public/app-assets/vendors/css/forms/select/select2.min.css">
+    <!-- END: Vendor CSS-->
+
+    <!-- BEGIN: Theme CSS-->
+    <link rel="stylesheet" type="text/css" href="<?php echo e(asset('public/app-assets/css/bootstrap.css')); ?>">
+
+
+</head>
+<body class="antialiased">
+<div id="app">
+
+    <!-- Your Vue component will be rendered here -->
+    <!-- Make sure your Vue component is properly compiled and included in the JavaScript file -->
+    <!-- For example, if your Vue component is named 'login_page', it should be rendered here -->
+
+    <router-view></router-view>
+</div>
+
+<!-- Include your compiled JavaScript file -->
+<script src="<?php echo e(asset('js/app.js')); ?>"></script>
+<!-- Include any additional scripts if needed -->
+</body>
+</html>
+<?php /**PATH D:\new xamp\htdocs\feedback-app\resources\views/welcome.blade.php ENDPATH**/ ?>
